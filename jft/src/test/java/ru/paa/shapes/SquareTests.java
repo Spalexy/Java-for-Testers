c04fc8cd2741ce424b4a1a92842b589f8c2930ff
Александр Придача <alex322p9@gmail.com>
2018-11-20 14:38:52 +0300

    Revert "Добавлен новый модуль в проект, добавлен тест из Katalon"
    This reverts commit d3362bf5ce6d44c19dd68bc91b9bf10b6b40f766.

--- jft/src/test/java/ru/paa/shapes/SquareTests.java
+++ jft/src/test/java/ru/paa/shapes/SquareTests.java
@@ -1,6 +1,5 @@
 package ru.paa.shapes;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
@@ -8,7 +7,6 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(3);
-    Assert.assertEquals(s.area(), 9.0);
+    assert s.area() == 9;
   }
-
 }
