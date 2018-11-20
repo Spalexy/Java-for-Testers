d3362bf5ce6d44c19dd68bc91b9bf10b6b40f766
Александр Придача <alex322p9@gmail.com>
2018-11-20 14:26:45 +0300

    Добавлен новый модуль в проект, добавлен тест из Katalon
    Добавлен модуль litecart-customers-web-tests
    Добавлен автосгенеренный тест из Katalon для страницы создания нового заказчика в litecart

--- jft/src/test/java/ru/paa/shapes/SquareTests.java
+++ jft/src/test/java/ru/paa/shapes/SquareTests.java
@@ -1,5 +1,6 @@
 package ru.paa.shapes;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
@@ -7,6 +8,7 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(3);
-    assert s.area() == 9;
+    Assert.assertEquals(s.area(), 9.0);
   }
+
 }
