7c903b46567511ebeee79a5dac7fb59d340eef1e
Александр Придача <alex322p9@gmail.com>
2018-11-20 16:23:15 +0300

    Удалил лишний код из класса CustomerCreationTest

--- litecart-customers-web-tests/src/test/java/ru/paa/litecart/customers/CustomerCreationTest.java
+++ litecart-customers-web-tests/src/test/java/ru/paa/litecart/customers/CustomerCreationTest.java
@@ -1,117 +1,63 @@
 package ru.paa.litecart.customers;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class CustomerCreationTest {
   private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
   public void testUntitledTestCase() throws Exception {
     driver.get("http://localhost/litecart/admin/login.php");
-    driver.findElement(By.name("username")).clear();
     driver.findElement(By.name("username")).sendKeys("admin");
-    driver.findElement(By.name("password")).clear();
     driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.name("login")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Currencies'])[1]/following::span[2]")).click();
+    driver.findElement(By.xpath("//span[contains(text(),'Customers')]")).click();
     driver.findElement(By.linkText("Add New Customer")).click();
     driver.findElement(By.name("code")).click();
-    driver.findElement(By.name("code")).clear();
     driver.findElement(By.name("code")).sendKeys("test");
     driver.findElement(By.name("email")).click();
-    driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("test");
+    driver.findElement(By.name("email")).sendKeys("test@test.ru");
     driver.findElement(By.name("tax_id")).click();
-    driver.findElement(By.name("tax_id")).clear();
     driver.findElement(By.name("tax_id")).sendKeys("test");
     driver.findElement(By.name("company")).click();
-    driver.findElement(By.name("company")).clear();
     driver.findElement(By.name("company")).sendKeys("test");
     driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).clear();
     driver.findElement(By.name("firstname")).sendKeys("test");
     driver.findElement(By.name("lastname")).click();
-    driver.findElement(By.name("lastname")).clear();
     driver.findElement(By.name("lastname")).sendKeys("test");
     driver.findElement(By.name("address1")).click();
-    driver.findElement(By.name("address1")).clear();
     driver.findElement(By.name("address1")).sendKeys("test");
     driver.findElement(By.name("address2")).click();
-    driver.findElement(By.name("address2")).clear();
     driver.findElement(By.name("address2")).sendKeys("test");
     driver.findElement(By.name("city")).click();
-    driver.findElement(By.name("city")).clear();
     driver.findElement(By.name("city")).sendKeys("test");
     driver.findElement(By.name("postcode")).click();
-    driver.findElement(By.name("postcode")).clear();
     driver.findElement(By.name("postcode")).sendKeys("test");
     driver.findElement(By.name("phone")).click();
-    driver.findElement(By.name("phone")).clear();
     driver.findElement(By.name("phone")).sendKeys("1234567");
     driver.findElement(By.name("mobile")).click();
-    driver.findElement(By.name("mobile")).clear();
     driver.findElement(By.name("mobile")).sendKeys("1234567");
     driver.findElement(By.name("new_password")).click();
-    driver.findElement(By.name("new_password")).clear();
     driver.findElement(By.name("new_password")).sendKeys("test");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Save'])[1]/i[1]")).click();
+    driver.findElement(By.name("save")).click();
   }
 
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
   }
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
