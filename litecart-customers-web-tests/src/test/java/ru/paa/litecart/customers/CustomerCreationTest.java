ae26557b6dc4b41d4d47342a1d5e5dff53d03b79
Александр Придача <alex322p9@gmail.com>
2018-11-20 17:07:07 +0300

    Параметризован fillNewCustomerForm()

--- litecart-customers-web-tests/src/test/java/ru/paa/litecart/customers/CustomerCreationTest.java
+++ litecart-customers-web-tests/src/test/java/ru/paa/litecart/customers/CustomerCreationTest.java
@@ -30,7 +30,9 @@ public class CustomerCreationTest {
   public void testCustomerCreation() throws Exception {
     goToCustomersPage();
     initCustomerCreation();
-    fillNewCustomerForm();
+    fillNewCustomerForm("test", "test@test.ru", "test", "test",
+            "test", "test", "test", "test", "test",
+            "test", "1234567", "1234567", "test");
     submitForm();
   }
 
@@ -38,33 +40,35 @@ public class CustomerCreationTest {
     driver.findElement(By.name("save")).click();
   }
 
-  private void fillNewCustomerForm() {
+  private void fillNewCustomerForm(String code, String email, String taxId, String company, String firstname,
+                                   String lastname, String address1, String address2, String city, String postcode,
+                                   String phone, String mobile, String password) {
     driver.findElement(By.name("code")).click();
-    driver.findElement(By.name("code")).sendKeys("test");
+    driver.findElement(By.name("code")).sendKeys(code);
     driver.findElement(By.name("email")).click();
-    driver.findElement(By.name("email")).sendKeys("test@test.ru");
+    driver.findElement(By.name("email")).sendKeys(email);
     driver.findElement(By.name("tax_id")).click();
-    driver.findElement(By.name("tax_id")).sendKeys("test");
+    driver.findElement(By.name("tax_id")).sendKeys(taxId);
     driver.findElement(By.name("company")).click();
-    driver.findElement(By.name("company")).sendKeys("test");
+    driver.findElement(By.name("company")).sendKeys(company);
     driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).sendKeys("test");
+    driver.findElement(By.name("firstname")).sendKeys(firstname);
     driver.findElement(By.name("lastname")).click();
-    driver.findElement(By.name("lastname")).sendKeys("test");
+    driver.findElement(By.name("lastname")).sendKeys(lastname);
     driver.findElement(By.name("address1")).click();
-    driver.findElement(By.name("address1")).sendKeys("test");
+    driver.findElement(By.name("address1")).sendKeys(address1);
     driver.findElement(By.name("address2")).click();
-    driver.findElement(By.name("address2")).sendKeys("test");
+    driver.findElement(By.name("address2")).sendKeys(address2);
     driver.findElement(By.name("city")).click();
-    driver.findElement(By.name("city")).sendKeys("test");
+    driver.findElement(By.name("city")).sendKeys(city);
     driver.findElement(By.name("postcode")).click();
-    driver.findElement(By.name("postcode")).sendKeys("test");
+    driver.findElement(By.name("postcode")).sendKeys(postcode);
     driver.findElement(By.name("phone")).click();
-    driver.findElement(By.name("phone")).sendKeys("1234567");
+    driver.findElement(By.name("phone")).sendKeys(phone);
     driver.findElement(By.name("mobile")).click();
-    driver.findElement(By.name("mobile")).sendKeys("1234567");
+    driver.findElement(By.name("mobile")).sendKeys(mobile);
     driver.findElement(By.name("new_password")).click();
-    driver.findElement(By.name("new_password")).sendKeys("test");
+    driver.findElement(By.name("new_password")).sendKeys(password);
   }
 
   private void initCustomerCreation() {
